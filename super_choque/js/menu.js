e6f989803dc94ca40329a04e228846e38725e758
leticia288 <leticia.o28@aluno.ifsc.edu.br>
2025-04-09 17:21:21 -0300

    up projeto

---
+++ super_choque/js/menu.js
@@ -0,0 +1,13 @@
+function mostrarMenu(){
+    const menuLateral = document.getElementById("menu-lateral");
+    menuLateral.classList.toggle('ativa');
+
+    const iconMenu = document.getElementById("img-menu");
+
+    if (iconMenu.src.endsWith('img/icon-hamburger-menu.png')){
+        iconMenu.src = "img/icon-close-menu.png";
+    } else {
+        iconMenu.src = "img/icon-hamburger-menu.png";
+    }
+
+}
